144d704151767aff3e185a8ed8a7cbea1b20155b
ZiqueYuutaka <izferren@gmail.com>
2017-02-15 18:20:37 -0500

    added GradeDisplayActivity

---
+++ app/src/main/java/com/testbed/zique_yuutaka/gradecalculator/GradeDisplayActivity.java
@@ -0,0 +1,13 @@
+package com.testbed.zique_yuutaka.gradecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GradeDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grade_display);
+    }
+}
